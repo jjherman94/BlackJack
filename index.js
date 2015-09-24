b8035b987e59d4fd843ad551db394541f34ae543
celvro <dwlfk2@mst.edu>
2015-09-24 16:51:45 -0500

    Added timestamps, also blank out input when entering text.

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+function getTime() {
+    var d = new Date(),
+        h = (d.getHours()<10?'0':'') + d.getHours(),
+        m = (d.getMinutes()<10?'0':'') + d.getMinutes(),
+        s = (d.getSeconds()<10?'0':'') + d.getSeconds();
+    return "["+h+":"+m+":"+s+"] ";
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    console.log('Client connected.');
+    console.log(getTime() + "Connection from " + socket.origin);
     var name = false;
     
     socket.on('chat message', function(msg){
         if ( name === false ){
             name = msg.replace(/ /g, "_");
             io.emit('chat message', name + ' connected.');
+            console.log(getTime() + socket.origin + ' identified as ' + name);
         } else {
             io.emit('chat message', name + ': ' + msg);
+            console.log(getTime() + name + "sent: " + msg);
         }
     });
     
     socket.on('disconnect', function() {
         if (name !== false) {
             io.emit('chat message', name + ' disconnected.');
+            console.log(getTime() + name + ' connected.');
         }
-        console.log(name + ' disconnected.');
     });
 });
 
